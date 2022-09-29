6f4db8912ba3ede586fd7ebf71e6567aaf60990d
isarenzetti <113146044+isarenz@users.noreply.github.com>
2022-09-29 16:32:07 +0200

    Create FrostyFriday_Challenge_Week1.sql

---
+++ FrostyFriday_Challenge_Week1.sql
@@ -0,0 +1,38 @@
+--Set the settings
+use database FROSTYFRIDAY;
+use schema FROSTYFRIDAYSCHEMA;
+
+--Create file format
+create or replace file format frostyfridayformat
+  type = 'CSV'
+  field_delimiter = '|'
+  skip_header = 1;
+  
+--create stage
+create or replace stage frostyfridaystage
+file_format = frostyfridayformat
+url = 's3://frostyfridaychallenges/challenge_1/';
+    
+  
+--check how many files have been staged
+list @frostyfridaystage;
+
+--Query metadata: how many columns do a table have?
+select metadata$filename
+, metadata$file_row_number
+, $1
+, $2
+from @frostyfridaystage;  
+  
+--Create table
+create or replace table week_1 (
+id varchar
+);
+  
+--Copy data from the stage into the table
+copy into week_1
+from @frostyfridaystage;
+
+--Check the table
+select *
+from week_1;
